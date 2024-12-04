0a2617cf8b5d75353d44c3a70586d908f3f8c552
boki <lsb530@naver.com>
2024-12-04 10:39:46 +0900

    테스트 환경 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,13 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 //    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+
+    // JUnit5 테스트 프레임워크
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
+
+    // 코루틴 테스트 라이브러리
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
 }
 
 tasks.test {
